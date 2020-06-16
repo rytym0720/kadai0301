8677960431387f5b18aeb157310d68068d5ee713
青山良太 <aoyamaryota@aoyamaryouta-no-MacBook-Air.local>
2020-06-16 21:52:20 +0900

    配列オブジェクトの値を空にしました。todoの定義する場所を上部にまとめました。taskの内容を取得した値の変数に変更し、addTaskの中にいれました。

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
             const addTaskTarget = document.getElementsByClassName('addTask-target')[0];
             const addTaskValue = document.getElementsByClassName('addTask-value')[0];  
             let nextId = 0;
+            const todos = [];
 
         const addTask = (task,id) => {
             //テーブル要素を生成する　td要素を作る
@@ -32,19 +33,18 @@
 
             //生成したbutton要素を生成する
             addButtonTd.append(addButton);
-            removeButtonTd.append(removeButton);       
+            removeButtonTd.append(removeButton);     
+            
+            const todo = {
+                task: 'taskSpanTd',
+                status: '作業中'
+            };
+            todos.push(todo);
+            
         };
 
-        const todos = [
-            {task: "test", status: "作業中"},
-            {task: "test2", status: "作業中"},
-            {task: "test3", status: "作業中"},
-        ];
-        const todo = {
-            task: "入力されたタスクの内容",
-            status: "作業中"
-        };
-        todos.push(todo);
+    
+   
 
         addTaskTrigger.addEventListener('click',() => {
             const task = addTaskValue.value;
