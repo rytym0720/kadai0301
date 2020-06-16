b1057f5c31a178c478fd889a8b41469608386e7b
青山良太 <aoyamaryota@aoyamaryouta-no-MacBook-Air.local>
2020-06-16 12:04:50 +0900

    ご指摘頂いたaddTask関数の内容の変更とオブジェクトを追加しました

--- js/main.js
+++ js/main.js
@@ -1,52 +1,54 @@
+  
+        document.addEventListener('DOMContentLoaded', function() {
+            const addTaskTrigger = document.getElementsByClassName('addTask-trigger')[0];
+            const addTaskTarget = document.getElementsByClassName('addTask-target')[0];
+            const addTaskValue = document.getElementsByClassName('addTask-value')[0];  
+            let nextId = 0;
 
-'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-    // 必要なDOM要素を取得。
-          const addTaskTrigger = document.getElementsByClassName('addTask-trigger')[0];
-          const addTaskTarget = document.getElementsByClassName('addTask-target')[0];
-          const addTaskValue = document.getElementsByClassName('addTask-value')[0];  
-          //ID用のインデックスを定義
-    let nextId = 0;
-    const addTask = (task,id) => {
-        // 表のタグを生成する
-        const tableItem=document.createElement('thead');
-        const addButton = document.createElement('button');
-        const removeButton = document.createElement('button');
-        addButton.style.margin = "5px"; 
-        removeButton.style.margin ="5px"; 
-        // それぞれ作業中、削除という言葉をボタンに入れる
-        addButton.innerText = '作業中';
-        removeButton.innerText = '削除';
-        //ボタンを押したら以下の作業をする
-        removeButton.addEventListener('click', () => removeTask(removeButton));
-        // IDを表示するspan要素を作成して tableItem に追加
-        const idSpan = document.createElement('span');
-        idSpan.innerText = id;
-        idSpan.style.marginRight = "20px"; 
-        tableItem.append(idSpan);
-    
-        const taskSpan = document.createElement('span');
-        taskSpan.style.width = "60px";  
-        taskSpan.style.display = "inline-block";
-        taskSpan.style.overflow = "hidden"; 
-        taskSpan.style.textOverflow = "ellipsis";
-        taskSpan.innerText = task;
-        taskSpan.title = task; 
-        tableItem.append(taskSpan);
-        
-    
-         //入力タスクを表示
-         addTaskTarget.appendChild(tableItem);   
-        // 作業中ボタンを追加
-        tableItem.appendChild(addButton);    
-        // 削除ボタンを追加
-        tableItem.appendChild(removeButton);
-    
-    };
-    // 追加ボタンに対して、タスク登録イベントを設定
-    addTaskTrigger.addEventListener('click', event => {
-        const task = addTaskValue.value;
-        addTask(task,nextId ++);
-        addTaskValue.value = '';
-    });
+        const addTask = (task,id) => {
+            //テーブル要素を生成する　td要素を作る
+            const tableItem = document.createElement('tr');
+            const addButtonTd = document.createElement('td');
+            const idSpanTd = document.createElement('td');
+            const taskSpanTd = document.createElement('td');
+            const removeButtonTd = document.createElement('td');
+
+            //Button要素を生成する
+            const addButton = document.createElement('button')
+            const removeButton = document.createElement('button')
+
+            //要素内のHTML文章を変更する
+            addButton.innerText = '作業中';
+            removeButton.innerText = '削除';
+            idSpanTd.innerText = id;
+            taskSpanTd.innerText = task;
+
+            //生成したテーブル要素をブラウザに表示する
+            tableItem.append(idSpanTd);
+            tableItem.append(taskSpanTd);
+            tableItem.append(addButtonTd);
+            tableItem.append(removeButtonTd);
+            addTaskTarget.appendChild(tableItem);
+
+            //生成したbutton要素を生成する
+            addButtonTd.append(addButton);
+            removeButtonTd.append(removeButton);       
+        };
+
+        const todos = [
+            {task: "test", status: "作業中"},
+            {task: "test2", status: "作業中"},
+            {task: "test3", status: "作業中"},
+        ];
+        const todo = {
+            task: "入力されたタスクの内容",
+            status: "作業中"
+        };
+        todos.push(todo);
+
+        addTaskTrigger.addEventListener('click',() => {
+            const task = addTaskValue.value;
+            addTask(task,nextId ++);
+            addTaskValue.value = '';
+        });
 });
