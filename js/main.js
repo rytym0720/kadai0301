ca8f413f6263511029c639d558f42d1d982d9068
青山良太 <aoyamaryota@aoyamaryouta-no-MacBook-Air.local>
2020-07-02 06:31:52 +0900

    ご指摘いただいた点修正しました

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-  
+
 {
   document.addEventListener('DOMContentLoaded', () => {
     const addTaskTrigger = document.getElementsByClassName('addTask-trigger')[0];
     const addTaskTarget = document.getElementsByClassName('addTask-target')[0];
     const addTaskValue = document.getElementsByClassName('addTask-value')[0];
     let nextId = 0;
+    const todos = [];
 
     //Taskとidを作成
     const addTask = (task, id, tableItem) => {
       let idSpanTd = document.createElement('td');
       let taskSpanTd = document.createElement('td');
+      //タスク追加時にtodosにtodoを追加 
+      const todo = {
+        task: 'taskSpanTd',
+        status: '作業中'
+      };
+      todos.push(todo);
       //要素内のHTML文章を変更する
       idSpanTd.innerText = id;
       taskSpanTd.innerText = task;
@@ -17,8 +24,8 @@
       tableItem.append(idSpanTd);
       tableItem.append(taskSpanTd);
       addTaskTarget.append(tableItem);
-      return (task, id);
     };
+
     //Button要素を生成する
     const addButton = (tableItem, removeButton, createButton) => {
       let createButtonTd = document.createElement('td');
@@ -33,8 +40,8 @@
       //生成したbutton要素を生成する
       createButtonTd.append(createButton);
       removeButtonTd.append(removeButton);
-      return (tableItem, removeButton, createButton);
     };
+
     //追加ボタンをクリックした際にtd要素を追加する処理を行う
     addTaskTrigger.addEventListener('click', () => {
       const task = addTaskValue.value;
@@ -46,21 +53,22 @@
       addTaskValue.value = '';
       removeButton.addEventListener('click', delete_element, false);
 
-    // //削除ボタンを押した時にタスクを削除する
-    function delete_element() {
-      let tableTag = this.closest('tr');if (tableTag)tableTag.remove();updateId();
-    }
+      // //削除ボタンを押した時にタスクを削除する
+      function delete_element() {
+        let tableTag = this.closest('tr');
+        if (tableTag) tableTag.remove(), updateId();
+      }
 
-    //　連番　再振り分け
-    let updateId = () => {
-      const tbody = document.getElementsByTagName("tbody")[0];
-      const taskList = tbody.getElementsByTagName('tr');
-      nextId = 0;
-      Array.from(taskList, tr => {
-        tr.getElementsByTagName('td')[0].textContent = nextId;
-        nextId++
-      });
-    }
+      //　連番　再振り分け
+      let updateId = () => {
+        const tbody = document.getElementsByTagName("tbody")[0];
+        const taskList = tbody.getElementsByTagName('tr');
+        nextId = 0;
+        Array.from(taskList, tr => {
+          tr.getElementsByTagName('td')[0].textContent = nextId;
+          nextId++
+        });
+      }
+    });
   });
-});
 }
